6855fc2aeebff2443f2628b58efaaa0d7bf2959c
Naomichi Kuroda <naomichi.kuroda.jp@gmail.com>
2018-05-21 02:05:51 +0900

    Add User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: {minimum: 6, maximum: 255}
+  validates :password, presence: true, length: {minimum: 8, maximum: 255}
+  validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_20_063726) do
+ActiveRecord::Schema.define(version: 2018_05_20_170006) do
 
   create_table "characters", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2018_05_20_063726) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "characters", "users"
