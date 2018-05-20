60351490d45a91af71cdf427edb8b7f211fa8e56
Naomichi Kuroda <naomichi.kuroda.jp@gmail.com>
2018-05-20 13:34:00 +0900

    Add User scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_19_074702) do
+ActiveRecord::Schema.define(version: 2018_05_20_030848) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
